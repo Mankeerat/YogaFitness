deb695051bd08931b8097475856aae78d0dbf293
Anele Mhlanga <aemhlanga@gmail.com>
2019-06-27 19:47:46 +0200

    Update 27 July 2019

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # YogaFitness
+
+Yoga Fitness is an app to measure , record and track your fitness
+
+## Icon
+<img src="art/icon.png" width="15%" />
+
+## Play Store Link
+**[Install on Google Play](https://play.google.com/store/apps/details?id=com.anelemhlanga.yogafitness)**
+
+## Art
+<img src="art/1.png" width="25%" />
+<img src="art/2.png" width="25%" />
+<img src="art/3.png" width="25%" />
+<img src="art/4.png" width="25%" />
+
+## Tools and Technologies
+* Java 
+
--- app/src/main/java/com/anelemhlanga/yogafitness/Views/LaunchingView.java
+++ app/src/main/java/com/anelemhlanga/yogafitness/Views/LaunchingView.java
@@ -1,5 +1,7 @@
 package com.anelemhlanga.yogafitness.Views;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,10 +11,24 @@ import io.fabric.sdk.android.Fabric;
 
 public class LaunchingView extends AppCompatActivity {
 
+    private static final int DISPLAY_TIME = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_launching_view);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LaunchingView.this, StartView.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        }, DISPLAY_TIME);
     }
 }
