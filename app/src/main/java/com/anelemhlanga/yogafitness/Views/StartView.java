65f0ce33fb4602683be0042b3ddcc48e86cba770
Anele Mhlanga <aemhlanga@gmail.com>
2019-07-22 09:36:29 +0200

    Created StartView UI

--- app/src/main/java/com/anelemhlanga/yogafitness/Views/StartView.java
+++ app/src/main/java/com/anelemhlanga/yogafitness/Views/StartView.java
@@ -1,7 +1,11 @@
 package com.anelemhlanga.yogafitness.Views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.anelemhlanga.yogafitness.R;
 
@@ -12,4 +16,29 @@ public class StartView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_view);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_calendar) {
+
+            return true;
+        } else if (id == R.id.action_settings){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
